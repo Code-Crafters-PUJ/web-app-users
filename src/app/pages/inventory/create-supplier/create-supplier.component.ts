e965a2469e7bb19f104ff353127eafbaa2283f5b
FabioB8a <fabiobuitra@gmail.com>
2024-04-24 20:56:45 -0500

    Add: Inventory components

---
+++ src/app/pages/inventory/create-supplier/create-supplier.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-supplier',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-supplier.component.html',
+  styleUrl: './create-supplier.component.css'
+})
+export class CreateSupplierComponent {
+
+}
