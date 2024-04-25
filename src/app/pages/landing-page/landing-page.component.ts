08d05dbc8886b2213368af24597aa4c201c073e8
FabioB8a <fabiobuitra@gmail.com>
2024-04-25 14:25:31 -0500

    Add: Landing

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuComponent } from './components/menu/menu.component';
 import { GeneralInfoComponent } from './components/general-info/general-info.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ServicesComponent } from './components/services/services.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MenuComponent, GeneralInfoComponent, AboutUsComponent],
+  imports: [MenuComponent, GeneralInfoComponent, AboutUsComponent, ServicesComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
