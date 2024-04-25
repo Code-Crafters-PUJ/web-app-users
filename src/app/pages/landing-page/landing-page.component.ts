0f31d3bd2c71bb5449ae9bc3a5bcc9dbf1f2ebb0
FabioB8a <fabiobuitra@gmail.com>
2024-04-25 00:13:57 -0500

    Add: About us

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from './components/menu/menu.component';
 import { GeneralInfoComponent } from './components/general-info/general-info.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MenuComponent, GeneralInfoComponent],
+  imports: [MenuComponent, GeneralInfoComponent, AboutUsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
