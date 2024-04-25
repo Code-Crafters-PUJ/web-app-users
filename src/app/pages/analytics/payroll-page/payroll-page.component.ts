106d5af2ad4dbc1c03c0c0a3b808dad98f47f437
FabioB8a <fabiobuitra@gmail.com>
2024-04-24 20:59:12 -0500

    Add: Analytics Components I

---
+++ src/app/pages/analytics/payroll-page/payroll-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payroll-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './payroll-page.component.html',
+  styleUrl: './payroll-page.component.css'
+})
+export class PayrollPageComponent {
+
+}
