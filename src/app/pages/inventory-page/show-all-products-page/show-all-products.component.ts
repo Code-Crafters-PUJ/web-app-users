85c1a403d0a568339b4d03e71d328f4fdf0faeaa
parraluisa <parran_ll@javeriana.edu.co>
2024-04-25 17:23:01 -0500

    inventory branch created

--- src/app/pages/inventory-page/show-all-products-page/show-all-products.component.ts
+++ src/app/pages/inventory-page/show-all-products-page/show-all-products.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SidebarComponent} from "../../../general/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-show-all-products',
   standalone: true,
-  imports: [],
+  imports: [
+    SidebarComponent
+  ],
   templateUrl: './show-all-products.component.html',
   styleUrl: './show-all-products.component.css'
 })
