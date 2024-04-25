faa26353ef7f3fcb5ae986ed60e7650e5df07bdf
FabioB8a <fabiobuitra@gmail.com>
2024-04-24 19:44:40 -0500

    Add: General Info Component

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from './components/menu/menu.component';
+import { GeneralInfoComponent } from './components/general-info/general-info.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MenuComponent],
+  imports: [MenuComponent, GeneralInfoComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
