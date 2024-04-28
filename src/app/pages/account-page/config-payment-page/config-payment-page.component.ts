f63db961f9e5ff3fab64ddeb93d6730ca8d27713
FabioB8a <fabiobuitra@gmail.com>
2024-04-28 17:30:05 -0500

    Add: Config Payment

--- src/app/pages/account-page/config-payment-page/config-payment-page.component.ts
+++ src/app/pages/account-page/config-payment-page/config-payment-page.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ActualPlanComponent } from './actual-plan/actual-plan.component';
+import { HistoricComponent } from './historic/historic.component';
 
 @Component({
   selector: 'app-config-payment-page',
   standalone: true,
-  imports: [],
+  imports: [ActualPlanComponent,HistoricComponent],
   templateUrl: './config-payment-page.component.html',
   styleUrl: './config-payment-page.component.css'
 })
