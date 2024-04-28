276b5d1c60c36d3eb99d4d30a13c56db335c3734
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-04-28 14:03:14 -0500

    show employees

--- src/app/pages/payroll-page/show-details-payroll-page/show-details-payroll-page.component.ts
+++ src/app/pages/payroll-page/show-details-payroll-page/show-details-payroll-page.component.ts
@@ -47,12 +47,12 @@ export class ShowDetailsPayrollPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.payrollId = this.route.snapshot.paramMap.get('id');
-    console.log('Payroll ID:', this.payrollId);  // Verifica que se esté obteniendo un ID válido
+    console.log('Payroll ID:', this.payrollId);
 
     this.payrollsService.getPayrollDetails(this.payrollId).subscribe(data => {
-      console.log('Data loaded:', data);  // Verifica que los datos se carguen correctamente
+      console.log('Data loaded:', data);
       this.payroll = data;
-      this.updateDisplayedEmployees();  // Asegúrate de llamar esto después de cargar los datos
+      this.updateDisplayedEmployees();
     }, error => {
       console.error('Error fetching payroll details', error);
     });
