a99427af052a4280ef8ae0c2384820de12760509
FabioB8a <fabiobuitra@gmail.com>
2024-04-29 10:26:07 -0500

    Add: Register Logic

--- src/app/pages/authentication-page/signup-i-page/info/info.component.ts
+++ src/app/pages/authentication-page/signup-i-page/info/info.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../../../services/login-services/auth.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-info',
   standalone: true,
   imports: [],
   templateUrl: './info.component.html',
-  styleUrl: './info.component.css'
+  styleUrls: ['./info.component.css']
 })
 export class InfoComponent {
 
+  constructor(private authService: AuthService) { }
+
+  signUpUser(accountData: any): void {
+
+    this.authService.signup(accountData).then((value) => {
+      if (value){
+        var jwt = JSON.parse(value).jwt;
+        // TO DO: Complete in case of Authentication
+      }
+      else {
+        this.handleFailedAuthentication();
+      }
+    })
+  }
+
+  private handleFailedAuthentication() {
+    Swal.fire({
+      title: 'Uppss algo pasó',
+      text: "Error en los datos ingresados, por favor verifique los campos",
+      icon: 'warning',
+      confirmButtonText: 'OK'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.reload();
+      }
+    });
+  }
+  
 }
