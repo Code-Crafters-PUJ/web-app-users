4f93cbf2b7616662891a9b48544b8acee77aa281
parraluisa <parran_ll@javeriana.edu.co>
2024-04-30 03:36:46 -0500

    Show all products updated

--- src/app/pages/inventory-page/show-all-products-page/show-all-products.component.ts
+++ src/app/pages/inventory-page/show-all-products-page/show-all-products.component.ts
@@ -1,15 +1,93 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {SidebarComponent} from "../../../general/sidebar/sidebar.component";
+import {CreateProductComponent} from "../create-product-page/create-product.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {Router} from "@angular/router";
+import {ProductService} from "../../../services/inventory-services/product/product.service";
+import {ReactiveFormsModule} from "@angular/forms";
+import {branch} from "../../../Models/Inventory/branch";
 
 @Component({
   selector: 'app-show-all-products',
   standalone: true,
   imports: [
-    SidebarComponent
+    SidebarComponent,
+    CreateProductComponent,
+    NgIf,
+    NgForOf,
+    ReactiveFormsModule
   ],
   templateUrl: './show-all-products.component.html',
   styleUrl: './show-all-products.component.css'
 })
-export class ShowAllProductsComponent {
+export class ShowAllProductsComponent implements OnInit{
+  protected readonly Math = Math;
+  branches: branch[] = [];
+  showProductRegister: boolean = false;
+  showProductbuy: boolean = false;
+  pagination: { [branchName: string]: { currentPage: number, pageSize: number, totalItems: number, totalPages:number } } = {};
+  constructor(
+    private productService: ProductService,
+    private router: Router
+  ) {
 
+  }
+
+  ngOnInit(): void {
+    this.productService.getAllBranchesByCompany(1).subscribe(branches => {
+      this.branches = branches;
+      this.initializePagination();
+    });
+  }
+
+  initializePagination() {
+    this.pagination = {};
+    for (const branch of this.branches) {
+      this.pagination[branch.name] = { currentPage: 1, pageSize: 10, totalItems: branch.products.length , totalPages: Math.ceil(branch.products.length / 10)};
+    }
+  }
+
+  changePage(branchName: string, direction: number) {
+    if (this.pagination[branchName]) {
+      const currentPage = this.pagination[branchName].currentPage;
+      const totalPages = Math.ceil(this.pagination[branchName].totalItems / this.pagination[branchName].pageSize);
+
+      this.pagination[branchName].currentPage = Math.max(1, Math.min(totalPages, currentPage + direction));
+    }
+  }
+
+  productDetails(id: number | null){
+    this.saveProduct(id);
+    this.router.navigate(['home/inventory/show/product/detail']);
+  }
+  saveProduct(id: number | null) {
+    if (id === null) {
+      return;
+    }
+    sessionStorage.setItem('productId', id.toString());
+  }
+
+  enableProductRegistrer() {
+    this.showProductRegister = true;
+
+  }
+
+  enablebuy() {
+    this.showProductbuy = true;
+
+  }
+
+  haandleProductDisabled(disabled: boolean) {
+    // Si el componente se desactiva, oculta la interfaz de creación de productos
+    this.showProductRegister = !disabled;
+
+    // Si el componente se desactiva, recargar o actualizar información
+    if (disabled) {
+      // Recargar sucursales o información de productos
+      this.productService.getAllBranchesByCompany(1).subscribe((branches) => {
+        this.branches = branches;
+        this.initializePagination();
+      });
+    }
+  }
 }
