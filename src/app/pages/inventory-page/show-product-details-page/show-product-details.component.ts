7397379ec07cce5d7c12d0f8a08dec2b01c3d8af
parraluisa <parran_ll@javeriana.edu.co>
2024-04-30 03:37:07 -0500

    Show product details updated

--- src/app/pages/inventory-page/show-product-details-page/show-product-details.component.ts
+++ src/app/pages/inventory-page/show-product-details-page/show-product-details.component.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CreateProductComponent} from "../create-product-page/create-product.component";
+import {product} from "../../../Models/Inventory/product";
+import {FormsModule} from "@angular/forms";
+import {branch} from "../../../Models/Inventory/branch";
+import {NgForOf} from "@angular/common";
+import {ProductService} from "../../../services/inventory-services/product/product.service";
+import {branchesProductTemplate} from "../../../Models/Inventory/branchesProductTemplate";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-show-product-details',
   standalone: true,
-  imports: [],
+  imports: [
+    CreateProductComponent,
+    FormsModule,
+    NgForOf
+  ],
   templateUrl: './show-product-details.component.html',
   styleUrl: './show-product-details.component.css'
 })
-export class ShowProductDetailsComponent {
 
+export class ShowProductDetailsComponent implements OnInit {
+  branches: branch[] =[];
+  product: product = {
+    id: 0,
+    name: '',
+    description: '',
+    category: '',
+    sellPrice: 0
+
+  };
+  branchesProducts: branchesProductTemplate[] = [];
+  productId: number = -1;
+  total=0;
+  constructor(
+    private productService: ProductService,
+    private router: Router
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    const productIdString = sessionStorage.getItem('productId');
+    if (productIdString !== null) {
+      this.productId = parseInt(productIdString);
+      this.productService.getProductById(this.productId).subscribe(product => {
+        this.product = product;
+      });
+      this.productService.getAllBranchesByProduct(this.productId).subscribe(branches => {
+        this.branchesProducts = branches;
+      });
+    }
+    sessionStorage.removeItem('supplierId');
+    //get the total of products of all branches
+    this.branchesProducts.forEach(branchProduct => {
+      this.total += branchProduct.quantity;
+    });
+  }
+
+  updateProduct(product: product): void {
+    this.product = product; // Actualizar el producto
+  }
+
+  cancelChanges() {
+    this.router.navigate(['home/inventory/show/product/all']);
+  }
+
+  deleteProduct(productId: number) {
+    this.productService.deleteProduct(productId).subscribe(() => {
+      this.router.navigate(['home/inventory/show/product/all']);
+    });
+
+  }
 }
