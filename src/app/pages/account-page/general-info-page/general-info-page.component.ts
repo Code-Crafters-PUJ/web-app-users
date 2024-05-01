4f80a858cd1d9c172340b1b3a11b6f7b027d1c90
FabioB8a <fabiobuitra@gmail.com>
2024-05-01 12:55:08 -0500

    Add: Sidebar Auth components

--- src/app/pages/account-page/general-info-page/general-info-page.component.ts
+++ src/app/pages/account-page/general-info-page/general-info-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { InfoPlanComponent } from './info-plan/info-plan.component';
 import { InfoProfileComponent } from './info-profile/info-profile.component';
 import { InfoSupportComponent } from './info-support/info-support.component';
+import { SidebarComponent } from '../../../general/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-general-info-page',
   standalone: true,
-  imports: [InfoPlanComponent,InfoProfileComponent,InfoSupportComponent],
+  imports: [InfoPlanComponent,InfoProfileComponent,InfoSupportComponent,SidebarComponent],
   templateUrl: './general-info-page.component.html',
   styleUrl: './general-info-page.component.css'
 })
