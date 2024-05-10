b6c9f111f9da67b06a32f3f8f940493e47b56975
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-05-09 23:32:28 -0500

    analytics

--- src/app/pages/analytics-page/sales-page/sales-page.component.ts
+++ src/app/pages/analytics-page/sales-page/sales-page.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Chart from "chart.js";
+import {SalesAnalyticService} from "../../../services/analytics-services/sales-analytic.service";
 
 @Component({
   selector: 'app-sales-page',
@@ -7,6 +9,100 @@ import { Component } from '@angular/core';
   templateUrl: './sales-page.component.html',
   styleUrl: './sales-page.component.css'
 })
-export class SalesPageComponent {
+export class SalesPageComponent implements OnInit {
+
+  public topSaledChart: any;
+  public incomeChart: any;
+  public branchChart: any;
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  constructor(private salesService: SalesAnalyticService) {}
+
+
+  loadData() {
+    this.salesService.getTopSaledProductsVsSales().subscribe(data => {
+      this.topSaledProductsVsSalesChart(data);
+    });
+
+    this.salesService.getMonthVsIncome().subscribe(data => {
+      this.monthVsIncomeChart(data);
+    });
+
+    this.salesService.getBranchVsSales().subscribe(data => {
+      this.branchVsSalesChart(data);
+    });
+  }
+
+  topSaledProductsVsSalesChart(data: { products: any; sales: any; }) {
+    this.topSaledChart = new Chart("topSaledProductsVsSales", {
+      type: 'bar',
+      data: {
+        labels: data.products,
+        datasets: [{
+          label: 'Productos con menos stock',
+          data: data.sales,
+          backgroundColor: [
+            'rgb(81,189,255)',
+            'rgb(81,189,255)',
+            'rgb(81,189,255)',
+            'rgb(81,189,255)',
+            'rgb(81,189,255)'
+          ]
+        }]
+      }
+    })
+  }
+
+
+  monthVsIncomeChart(data: { months: any; income: any; }) {
+    this.incomeChart = new Chart("monthVsIncome", {
+      type: 'line',
+      data: {
+        labels: data.months,
+        datasets: [{
+          label: 'Salario Quincenal',
+          data: data.income,
+          borderColor: 'rgb(81,189,255)',
+          fill: false,
+          pointRadius: 2,
+          pointHitRadius: 3
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
+
+
+  branchVsSalesChart(data: { branches: any; sales: any; }) {
+    this.branchChart = new Chart("branchVsSales", {
+      type: 'bar',
+      data: {
+        labels: data.branches,
+        datasets: [{
+          label: 'Productos con menos stock',
+          data: data.sales,
+          backgroundColor: [
+            'rgb(81,189,255)',
+            'rgb(81,189,255)',
+            'rgb(81,189,255)',
+            'rgb(81,189,255)',
+            'rgb(81,189,255)'
+          ]
+        }]
+      }
+    })
+  }
 
 }
--- src/app/services/analytics-services/inventory-analytic.service.ts
+++ src/app/services/analytics-services/inventory-analytic.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class InventoryAnalyticService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000/inventory';
+
+  constructor(private http: HttpClient ) { }
+
+  getcategoryVsStockPercentage(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/category-vs-stock-percentage`);
+  }
+
+  getproductVsTop5lessStock(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/product-vs-top-5-less-stock`);
+  }
+
+
+  getproviderVsStockPercentage(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/provider-vs-stock-percentage`);
+  }
+
+
 }
+
