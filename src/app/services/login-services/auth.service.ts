7380d20951fdb826fbf84c8e044eeacb54b15ba0
FabioB8a <fabiobuitra@gmail.com>
2024-05-11 10:26:18 -0500

    Add: Config back

--- src/app/services/login-services/auth.service.ts
+++ src/app/services/login-services/auth.service.ts
@@ -15,7 +15,6 @@ export class AuthService {
   async login(email: string, password: string) {
     var responseBody = await this.http.post(`${environment.baseURL}/user/login`, { email: email, password: password }, { withCredentials: false, responseType: "text" }).toPromise();
     return responseBody;
-
   }
 
   async signup(account: Account) {
