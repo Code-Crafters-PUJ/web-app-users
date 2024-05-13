d4e7af544faee6e78a4ff9da2a650e9de1dbbe45
parraluisa <parran_ll@javeriana.edu.co>
2024-05-12 22:35:16 -0500

    Update: account service

--- src/app/services/account-services/account.service.ts
+++ src/app/services/account-services/account.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {Plan} from "../../Models/user-models/plan";
+import {Plan} from "../../models/user-models/plan";
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs/internal/Observable";
 import {of} from "rxjs";
@@ -13,42 +13,48 @@ export class AccountService {
     {
       id: 1,
       type: 'Plan 1',
-      price: 100,
+      mensual_price: 100,
+      semestral_price: 500,
+      anual_price: 1000,
       plan_description: 'Descripción 1',
       maxUsers: 10
     },
     {
       id: 2,
       type: 'Plan 2',
-      price: 200,
+      mensual_price: 200,
+      semestral_price: 1000,
+      anual_price: 2000,
       plan_description: 'Descripción 2',
       maxUsers: 20
     },
     {
       id: 3,
       type: 'Plan 3',
-      price: 300,
+      mensual_price: 300,
+      semestral_price: 1500,
+      anual_price: 3000,
       plan_description: 'Descripción 3',
       maxUsers: 30
     },
     {
       id: 4,
       type: 'Plan 4',
-      price: 400,
+      mensual_price: 400,
+      semestral_price: 2000,
+      anual_price: 4000,
       plan_description: 'Descripción 4',
       maxUsers: 40
     }
   ];
 
-  companies: Company[] = [
+  companies: any[] = [
     {
       id: 1,
       NIT: '800.328',
       name: 'empresa 1',
       businessArea: 'software',
       employeeNumber: 200,
-      electronicPayroll: 928340,
-      electronicBill: 2893749,
       planId: 1,
     },
     {
@@ -92,24 +98,105 @@ export class AccountService {
       planId: 1,
     }
   ];
+  branches = [
+    {
+      id: 1,
+      name: 'sucursal 1',
+      address: 'calle 1',
 
-  company: Company = {
+    },
+    {
+      id: 2,
+      name: 'sucursal 2',
+      address: 'calle 2',
+    },
+    {
+      id: 3,
+      name: 'sucursal 3',
+      address: 'calle 3',
+    },
+    {
+      id: 4,
+      name: 'sucursal 4',
+      address: 'calle 4',
+    },
+    {
+      id: 5,
+      name: 'sucursal 5',
+      address: 'calle 5',
+    },
+  ];
+
+  company: any = {
     id: 0,
     NIT: '',
     name: '',
     businessArea: '',
     employeeNumber: 0,
-    electronicPayroll: 0,
-    electronicBill: 0,
     planId: 0,
   }
   plan: Plan = {
     id: 0,
     type: '',
-    price: 0,
+    mensual_price: 0,
+    semestral_price: 0,
+    anual_price: 0,
     plan_description: '',
     maxUsers: 0
   }
+
+  historic: any[] = [
+    {
+      id: 1,
+      initial_date: '2021-01-01',
+      final_date: '2021-01-31',
+      amount: 100,
+      active: true,
+      payment_date: '2021-01-01',
+      payment_method: 'tarjeta de credito',
+      plan: 'Plan 1'
+    },
+    {
+      id: 2,
+      initial_date: '2021-02-01',
+      final_date: '2021-02-28',
+      amount: 100,
+      active: true,
+      payment_date: '2021-02-01',
+      payment_method: 'tarjeta de credito',
+      plan: 'Plan 1'
+    },
+    {
+      id: 3,
+      initial_date: '2021-03-01',
+      final_date: '2021-03-31',
+      amount: 100,
+      active: true,
+      payment_date: '2021-03-01',
+      payment_method: 'tarjeta de credito',
+      plan: 'Plan 1'
+    },
+    {
+      id: 4,
+      initial_date: '2021-04-01',
+      final_date: '2021-04-30',
+      amount: 100,
+      active: true,
+      payment_date: '2021-04-01',
+      payment_method: 'tarjeta de credito',
+      plan: 'Plan 2'
+    },
+    {
+      id: 5,
+      initial_date: '2021-05-01',
+      final_date: '2021-05-31',
+      amount: 100,
+      active: true,
+      payment_date: '2021-05-01',
+      payment_method: 'tarjeta de credito',
+      plan: 'Plan 2'
+    }
+  ];
 
   constructor(
     private http: HttpClient
@@ -150,6 +237,8 @@ export class AccountService {
     let profile = {
       name: "Fabio",
       lastname: "Cuevas",
+      typeCardId: "C.C",
+      cardId: "123456",
       phone: "3510020",
       email: "fabio@kajfn.jslf",
       password: "jsjdf",
@@ -161,40 +250,85 @@ export class AccountService {
   }
 
   getAllBranchesByCompany(companyId: number): Observable<any> {
-    let branches = [
-      {
-        id: 1,
-        name: 'sucursal 1',
-        address: 'calle 1',
 
-      },
-      {
-        id: 2,
-        name: 'sucursal 2',
-        address: 'calle 2',
-      },
-      {
-        id: 3,
-        name: 'sucursal 3',
-        address: 'calle 3',
-      },
-      {
-        id: 4,
-        name: 'sucursal 4',
-        address: 'calle 4',
-      },
-      {
-        id: 5,
-        name: 'sucursal 5',
-        address: 'calle 5',
-      },
-    ];
-    return of(branches);
+    return of(this.branches);
   }
 
   changePassword(companyId: number, newPassword: string, actualPassword: string):Observable<any> {
     let okActualPassword = false;
     //answer = this.http.post(environment.baseURL + "/changePassword", {companyId, newPassword, actualPassword});
     return of(true);
+  }
+
+  deleteBranch(id: number, companyId: number):Observable<any> {
+    this.branches.splice(this.branches.findIndex(branch => branch.id === id), 1);
+    return of(true);
+  }
+
+  generatebranchId(companyId: number) {
+    return this.branches.length + 1;
+  }
+
+  companyNitExists(NIT: number, companyId: number) {
+    let company = this.companies.find(company => company.NIT === NIT && company.id !== companyId);
+    if (company) {
+      return of(true);
+    }
+    return of(false);
+  }
+
+  updateCompany(company: {
+    businessArea: string;
+    NIT: number;
+    name: string;
+    planId: number;
+    id: number;
+    employeeNumber: number
+  }) {
+    //Replace th company searching by id or nit
+    let companyIndex = this.companies.findIndex(comp => comp.id === company.id);
+    if (companyIndex === -1) {
+      companyIndex = this.companies.findIndex(comp => comp.NIT === company.NIT);
+    }
+    this.companies[companyIndex] = company;
+    return of(true);
+  }
+
+  updateRootAccount(rootProfile: {
+    password: string;
+    role: string;
+    phone: string;
+    cardId: string;
+    name: string;
+    typeCardId: string;
+    email: string;
+    businessNit: string;
+    lastname: string
+  }) {
+    return of(true);
+  }
+
+  updateBranches(branches: { address: string; name: string; id: number }[], companyId: number) {
+    branches.forEach(newBranch => {
+      let existingBranch = this.branches.find(branch => branch.id === newBranch.id);
+      if (existingBranch) {
+        console.log('Updating branch');
+        console.log(existingBranch);
+        existingBranch.address = newBranch.address;
+        existingBranch.name = newBranch.name;
+        console.log(existingBranch);
+      } else {
+        // Add new branch
+        console.log('Adding new branch');
+        console.log(newBranch);
+        this.branches.push(newBranch);
+      }
+    });
+    return of(true);
+  }
+
+  getHistoricByCompanyId(companyId: number) {
+
+    return of(this.historic);
   }
 }
