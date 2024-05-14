ecb7427aa31b5bd10801e13a8578ef8e1fc2a2ea
FabioB8a <fabiobuitra@gmail.com>
2024-05-14 17:12:25 -0500

    Add: Styles

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -5,11 +5,12 @@ import { AboutUsComponent } from './components/about-us/about-us.component';
 import { ServicesComponent } from './components/services/services.component';
 import { PlansComponent } from './components/plans/plans.component';
 import { AskAnythingComponent } from './components/ask-anything/ask-anything.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [MenuComponent, GeneralInfoComponent, AboutUsComponent, ServicesComponent, PlansComponent, AskAnythingComponent],
+  imports: [MenuComponent, GeneralInfoComponent, AboutUsComponent, ServicesComponent, PlansComponent, AskAnythingComponent, FooterComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
