cd83c7698b02ff2ed32c45071d786ed0a1b59764
FabioB8a <fabiobuitra@gmail.com>
2024-05-15 15:36:46 -0500

    Finish: Support page

--- src/app/pages/account-page/general-info-page/info-support/info-support.component.ts
+++ src/app/pages/account-page/general-info-page/info-support/info-support.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Company } from '../../../../models/user-models/company';
+import { AccountService } from '../../../../services/account-services/account.service';
 
 @Component({
   selector: 'app-info-support',
@@ -9,4 +11,45 @@ import { Component } from '@angular/core';
 })
 export class InfoSupportComponent {
 
+  companyId: number = -1;
+  company: Company = {
+    NIT: '',
+    name: '',
+    businessArea: '',
+    employeeNumber: 0,
+  };
+  rootAccount = {
+    name: "",
+    lastname: "",
+    typeCardId: "",
+    cardId: "",
+    phone: "",
+    email: "",
+    password: "",
+    businessNit: "",
+    role: "",
+  };
+
+  constructor(
+    private accountService: AccountService
+  ) {
+  }
+
+  ngOnInit(): void {
+    let companyIdString = sessionStorage.getItem('companyId');
+    if (companyIdString != null) {
+      this.companyId = parseInt(companyIdString);
+    }
+    //TODO: Remove companyId manual assignment
+    this.companyId = 1;
+    this.accountService.getCompanyById(this.companyId).subscribe((company: Company) => {
+      this.company = company;
+    });
+    this.accountService.getRootAccount(this.companyId).subscribe(account => {
+      this.rootAccount = account;
+    });
+
+
+  }
+  
 }
