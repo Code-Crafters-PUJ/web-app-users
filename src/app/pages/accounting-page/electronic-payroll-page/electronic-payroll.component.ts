21899fb5b4775258f2942ecb6e430d2b644fa174
JuanLesmes <juanfernandolesmescastaneda@gmail.com>
2024-05-15 15:14:07 -0500

    add electronicPayroll Page

--- src/app/pages/accounting-page/electronic-payroll-page/electronic-payroll.component.ts
+++ src/app/pages/accounting-page/electronic-payroll-page/electronic-payroll.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
-import {SidebarComponent} from "../../../general/sidebar/sidebar.component";
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from '../../../general/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-electronic-payroll',
   standalone: true,
-  imports: [
-    SidebarComponent
-  ],
+  imports: [CommonModule,SidebarComponent],
   templateUrl: './electronic-payroll.component.html',
   styleUrl: './electronic-payroll.component.css'
 })
 export class ElectronicPayrollComponent {
+
+  fileUploaded = false;
+
+  onUploadButtonClick() {
+    const fileInput = document.getElementById('fileInput') as HTMLInputElement;
+    if (fileInput) {
+      fileInput.click();
+    }
+  }
+
+  onFileSelected() {
+    this.fileUploaded = true;
+  }
+
+  downloadPDF() {
+    const pdfBlob = new Blob([' '], { type: 'application/pdf' });
+    const url = window.URL.createObjectURL(pdfBlob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'carta_de_exoneracion.pdf';
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
 
 }
